6a596e1a8dbe91092a944ccb8171296bf317907e
Kerem Ozkilic <kerem@fireflyon.com>
2019-06-19 15:05:24 +0300

    first push

---
+++ main.py
@@ -0,0 +1,32 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_flex_quickstart]
+import logging
+
+import hug
+
+
+app = hug.API(__name__)
+
+
+@hug.get(examples='name=Timothy&age=26')
+@hug.local()
+def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    """Says happy birthday to a user"""
+    return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
+            'took': float(hug_timer)}
+
+if __name__ == '__main__':
+    app.http.serve()
